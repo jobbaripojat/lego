bc3ad48cf7a1dd0c30749b24d8a2e902dbdcc97b
laury <lauri.ryopponen@gmail.com>
2021-03-18 12:42:02 +0200

    javadoc

---
+++ MyFirstDublo/src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"app","l":"ColorDetection"},{"p":"app.library","l":"ColorSensor"},{"p":"app","l":"FollowLine"},{"p":"app","l":"Movement"},{"p":"app","l":"ObjectDetection"},{"p":"app","l":"SoundPlay"},{"p":"app.library","l":"UltraSonicSensor"}];updateSearchResults();
